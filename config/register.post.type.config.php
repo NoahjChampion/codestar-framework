bb7ad0eaa79e2c693de24768cdffaf133ed87f23
Noahj Champion <NoahjChampion@users.noreply.github.com>
2016-08-01 16:14:50 -0400

    Updated instructional notes...
    Updated instructional note typos and to be more specifc.

--- config/register.post.type.config.php
+++ config/register.post.type.config.php
@@ -1,14 +1,14 @@
 <?php if ( ! defined( 'ABSPATH' ) ) { die; } // Cannot access pages directly.
 // @NOTE
-//  Please make sure your closing function does not have a comma after it.
-//  ie: the array before your closing function should not have a comma.
+//  Please make sure your closing function does not have a comma before it.
+//  ie: your final array before your closing function should not have a comma after it.
 // Adding a custom post type
 cs_post_type('custom-post-type',
     array(
         'singular' => 'Demo Post Type',
         'plural' => 'Demo Post Type',
         'slug' => 'demo-post-type',
-    )
+    ) // <= No comma after this point or error will trigger
 );
 // Adding a custom taxonomy to a post type
 cs_taxonomy('Categories', 'custom-post-type',
@@ -16,5 +16,5 @@ cs_taxonomy('Categories', 'custom-post-type',
         'singular' => 'Categories',
         'plural' => 'Category',
         'slug' => 'category',
-    )
+    ) // <= No comma after this point or error will trigger
 );
