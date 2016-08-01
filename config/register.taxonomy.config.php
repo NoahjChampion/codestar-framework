7d4e821d185a2a284247ee3e19c6d567107afda0
Noahj Champion <NoahjChampion@users.noreply.github.com>
2016-08-01 16:17:15 -0400

    Add files via upload

---
+++ config/register.taxonomy.config.php
@@ -0,0 +1,4 @@
+<?php if ( ! defined( 'ABSPATH' ) ) { die; } // Cannot access pages directly.
+// @NOTE
+//  You will probably want to keep your custom taxonomies along with your custom post types. 
+//  So this file will probably work best when adding custom taxonomies to already existing or another plugin's custom post type
